ee29a07cf0a88ab832c0e7c370485a9102b461f0
it22563996 <it22563996@my.sliit.lk>
2024-04-29 02:53:03 +0530

    new changes in customize pack

--- router/custompackageRouter.js
+++ router/custompackageRouter.js
@@ -1,9 +1,12 @@
 import { Router } from "express";
-import { addcustomizePackage, getAllCustomPackage } from "../controller/customPackageController.js";
+import { addcustomizePackage, allCustomPack, updatepackStatus} from "../controller/customPackageController.js";
 
 const router = Router();
 router.route("/addcustompackage").post(addcustomizePackage);
-router.route("/getallcustompackage").get(getAllCustomPackage);
+router.route("/getallcustompackage").get(allCustomPack);
+router.route("/detelecustompack/:id").put(updatepackStatus);
+
+
 
 
 
